ef006f7087ad2061f8e5a7323e0bda7d8efcf84e
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-04-17 00:13:22 -0300

    making Working with relational databases in Python module - pt 2

--- 01_Courses/09_Introduction to Importing Data in Python/03_Working with relational databases in Python/example_04.py
+++ 01_Courses/09_Introduction to Importing Data in Python/03_Working with relational databases in Python/example_04.py
@@ -7,3 +7,26 @@ engine = create_engine('sqlite:///' + file_path)
 df = pd.read_sql_query("SELECT OrderID, CompanyName FROM Orders INNER JOIN  Custumers on Orders.CustumerID = Custumers.CustumerID",engine)
 print(df.head())
 '''
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+file_path = './data/Chinook.sqlite'
+engine = create_engine("sqlite:///" + file_path)
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+
+print("*" * 60)
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000",engine)
+
+# Print head of DataFramea
+print(df.head())
